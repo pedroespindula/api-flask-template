6cd42a8097e3b2337f41275579a79e1c39dc2524
Pedro Espíndula <joaopedro.jpse@gmail.com>
2020-07-06 12:30:41 -0300

    Adiciona testes para a rota de ping

--- src/tests/unit/routes/test_ping.py
+++ src/tests/unit/routes/test_ping.py
@@ -1,11 +1,33 @@
+# pylint: disable=W0611,W0621
+
 from tests.fixtures import client
 
 def test_ping_status_code(client):
     response = client.get('/ping/')
 
-    assert response.status_code == 200, 'Should be true'
+    assert response.status_code == 200, 'Should be 200'
 
 def test_ping_body(client):
     response = client.get('/ping/')
 
-    assert response.data.decode('utf-8') == 'ping', 'Should be true'
+    assert response.data.decode('utf-8') == 'ping', 'Should return "ping"'
+
+def test_ping_cannot_post(client):
+    response = client.post('/ping/')
+
+    assert response.status_code == 405, 'Should not allow POST method'
+
+def test_ping_cannot_put(client):
+    response = client.put('/ping/')
+
+    assert response.status_code == 405, 'Should not allow PUT method'
+
+def test_ping_cannot_patch(client):
+    response = client.patch('/ping/')
+
+    assert response.status_code == 405, 'Should not allow PATCH method'
+
+def test_ping_cannot_delete(client):
+    response = client.delete('/ping/')
+
+    assert response.status_code == 405, 'Should not allow DELETE method'
